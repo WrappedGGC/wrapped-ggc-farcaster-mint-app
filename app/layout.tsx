be114eed987e28edf86180b4a7c5b65534b9cf06
Tickether <tickether@gmail.com>
2025-11-23 10:56:38 +0000

    feat: Update miniapp assets and metadata integration
    Added opengraph and logo images, updated Farcaster miniapp metadata, and removed unused SVG assets. Updated layout.tsx to reference new image URLs and improved miniapp configuration for Farcaster integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,14 +26,14 @@ export default function RootLayout({
 }>) {
   const miniApp = {
     version: "next",
-    imageUrl: "",
+    imageUrl: "https://wrapped-ggc-farcaster-mint-app.vercel.app/opengraph-image.png",
     button: {
       title: "Invest in Gold w/ WrappedGGC",
       action: {
         type: "launch_miniapp",
-        url: "",
+        url: "https://wrapped-ggc-farcaster-mint-app.vercel.app",
         name:"Wrapped GGC",
-        splashImageUrl: "",
+        splashImageUrl: "https://wrapped-ggc-farcaster-mint-app.vercel.app/icons/rounded_logo.png",
         splashBackgroundColor:"#ffffff"
       }
     }
@@ -44,7 +44,7 @@ export default function RootLayout({
       <head>
         <meta 
         property="og:image" 
-          content="" 
+          content="https://wrapped-ggc-farcaster-mint-app.vercel.app/opengraph-image.png" 
         />
         <meta 
           name="fc:miniapp"
@@ -52,7 +52,7 @@ export default function RootLayout({
         />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistMono.className}`}
       >
         <WagmiContext>
           <ConnectContext>
